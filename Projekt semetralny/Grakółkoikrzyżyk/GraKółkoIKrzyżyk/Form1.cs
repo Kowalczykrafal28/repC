a40ef8c56c09f2de7d5b3aecaa6033e9da1f3b1f
Kowalczykrafal28 <48623698+Kowalczykrafal28@users.noreply.github.com>
2019-06-24 20:54:34 +0200

    usprawnieni gry - blokowanie przyciskow

--- Projekt semetralny/Grakółkoikrzyżyk/GraKółkoIKrzyżyk/Form1.cs
+++ Projekt semetralny/Grakółkoikrzyżyk/GraKółkoIKrzyżyk/Form1.cs
@@ -17,83 +17,93 @@ namespace GraKółkoIKrzyżyk
             InitializeComponent();
         }
 
-        
-       
+
+        bool blokada = true;
         int liczba_klikniec = 0 ;
         int liczba_ruchów = 0;
+        
 
         private void button1_Click(object sender, EventArgs e)
+
         {
-            if (liczba_klikniec % 2==0)
+
+            if (liczba_klikniec % 2 == 1 && button1.Text != "X" && blokada== true)
+            {
+                button1.Text = "O";
+                liczba_klikniec++;
+                label3.Text = "Następny ruch : X";
+            }
+
+            if (liczba_klikniec % 2 == 0 && button1.Text != "O" && blokada == true)
             {
                 button1.Text = "X";
                 label3.Text = "Następny ruch : O";
                 liczba_klikniec++;
             }
-           
-            else if ( liczba_klikniec %2 == 1)
+
+            if (button3.Text == "X" && button2.Text == "X" && button1.Text == "X")
             {
-                button1.Text = "O";
-                label3.Text = "Następny ruch : X";
-                liczba_klikniec++;
-            }
-            else if (button3.Text == "O" && button2.Text == "O" && button1.Text == "O")
-            {
-                label3.Text = " Wygrał O";
+                label3.Text = " Gre wygrał gracz X";
+                blokada = false;
             }
 
-
-            liczba_klikniec++;
-            liczba_ruchów ++;
+            liczba_ruchów++;
             label1.Text = " Liczba ruchów : " + liczba_ruchów;
-             
+
         }
 
         private void button3_Click(object sender, EventArgs e)
-        {
-            if (liczba_klikniec % 2 == 0)
+
+        {     
+
+            if (liczba_klikniec % 2 == 1 && button3.Text != "X"  && blokada == true)  
+            {
+                button3.Text = "O";
+                liczba_klikniec++;
+                label3.Text = "Następny ruch : X";
+            }
+         
+            if (liczba_klikniec % 2 == 0 && button3.Text != "O" && blokada == true)
             {
                 button3.Text = "X";
                 label3.Text = "Następny ruch : O";
                 liczba_klikniec++;
             }
-           
-            else if (liczba_klikniec % 2 == 1)
+
+            if (button3.Text == "X" && button2.Text == "X" && button1.Text == "X")
             {
-                button3.Text = "O";
-                label3.Text = "Następny ruch : X";
-                liczba_klikniec++;
-            }
-            else if (button3.Text == "O" && button2.Text == "O" && button1.Text == "O")
-            {
-                label3.Text = " Wygrał O";
+                label3.Text = " Gre wygrał gracz X";
+                blokada = false;
             }
 
             liczba_ruchów++;
-            label1.Text = " Liczba ruchów : " + liczba_ruchów;
+            label1.Text = " Liczba ruchów : " + liczba_ruchów; 
             
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            if (liczba_klikniec % 2 == 0)
+           
+
+            if (liczba_klikniec % 2 == 1 && button6.Text != "X" && blokada == true)  
+            {
+                button6.Text = "O";
+                liczba_klikniec++;
+                label3.Text = "Następny ruch : X";
+            }
+         
+            if (liczba_klikniec % 2 == 0 && button6.Text != "O" && blokada == true)
             {
                 button6.Text = "X";
                 label3.Text = "Następny ruch : O";
                 liczba_klikniec++;
             }
 
-            else if (liczba_klikniec % 2 == 1)
+            if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
             {
-                button6.Text = "O";
-                liczba_klikniec++;
-                label3.Text = "Następny ruch : X";
+                label3.Text = " Gre wygrał gracz X";
+                blokada = false;
             }
-            else if (button3.Text == "O" && button2.Text == "O" && button1.Text == "O")
-            {
-                label3.Text = " Wygrał O";
-            }
-
 
             liczba_ruchów++;
             label1.Text = " Liczba ruchów : " + liczba_ruchów; 
@@ -102,139 +112,172 @@ namespace GraKółkoIKrzyżyk
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (liczba_klikniec % 2 == 0)
+
+            if (liczba_klikniec % 2 == 1 && button2.Text != "X" && blokada == true)
+            {
+                button2.Text = "O";
+                liczba_klikniec++;
+                label3.Text = "Następny ruch : X";
+            }
+
+            if (liczba_klikniec % 2 == 0 && button2.Text != "O" && blokada == true)
             {
                 button2.Text = "X";
                 label3.Text = "Następny ruch : O";
                 liczba_klikniec++;
             }
-            else if (button3.Text == "X" && button2.Text == "X" && button1.Text == "X")
+
+            if (button3.Text == "X" && button2.Text == "X" && button1.Text == "X")
             {
-                label3.Text = " Wygrał X";
+                label3.Text = " Gre wygrał gracz X";
+                blokada = false;
             }
-            else if (liczba_klikniec % 2 == 1)
-            {
-                button2.Text = "O";
-                label3.Text = "Następny ruch : X";
-                liczba_klikniec++;
-            }
-            
+
             liczba_ruchów++;
             label1.Text = " Liczba ruchów : " + liczba_ruchów;
 
         }
-
         private void button5_Click(object sender, EventArgs e)
         {
-            if (liczba_klikniec % 2 == 0)
+
+
+            if (liczba_klikniec % 2 == 1 && button5.Text != "X" && blokada == true)
+            {
+                button5.Text = "O";
+                liczba_klikniec++;
+                label3.Text = "Następny ruch : X";
+            }
+
+            if (liczba_klikniec % 2 == 0 && button5.Text != "O" && blokada == true)
             {
                 button5.Text = "X";
                 label3.Text = "Następny ruch : O";
                 liczba_klikniec++;
             }
-            else if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
+
+            if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
             {
-                label3.Text = " Wygrał X";
+                label3.Text = " Gre wygrał gracz X";
             }
-            else if (liczba_klikniec % 2 == 1)
-            {
-                button5.Text = "O";
-                label3.Text = "Następny ruch : X";
-                liczba_klikniec++;
-            }
-          
 
             liczba_ruchów++;
             label1.Text = " Liczba ruchów : " + liczba_ruchów;
+            blokada = false;
+
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            if (liczba_klikniec % 2 == 0)
+
+
+            if (liczba_klikniec % 2 == 1 && button4.Text != "X"  && blokada == true)
+            {
+                button4.Text = "O";
+                liczba_klikniec++;
+                label3.Text = "Następny ruch : X";
+            }
+
+            if (liczba_klikniec % 2 == 0 && button4.Text != "O" && blokada == true)
             {
                 button4.Text = "X";
                 label3.Text = "Następny ruch : O";
                 liczba_klikniec++;
             }
-            else if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
+
+            if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
             {
-                label3.Text = " Wygrał X";
+                label3.Text = " Gre wygrał gracz X";
+                blokada = false;
             }
-            else if (liczba_klikniec % 2 == 1)
-            {
-                button4.Text = "O";
-                label3.Text = "Następny ruch : X";
-                liczba_klikniec++;
-            }
-            
+
             liczba_ruchów++;
             label1.Text = " Liczba ruchów : " + liczba_ruchów;
-        }
 
+        }
         private void button9_Click(object sender, EventArgs e)
         {
-            if (liczba_klikniec % 2 == 0)
-            {
-                button9.Text = "X";
-                liczba_klikniec++;
-                label3.Text = "Następny ruch : O";
-            }
-            else if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
-            {
-                label3.Text = " Wygrał X";
-            }
-            else if (liczba_klikniec % 2 == 1)
+
+
+            if (liczba_klikniec % 2 == 1 && button9.Text != "X" && blokada == true)
             {
                 button9.Text = "O";
+                liczba_klikniec++;
                 label3.Text = "Następny ruch : X";
+            }
+
+            if (liczba_klikniec % 2 == 0 && button9.Text != "O"  && blokada == true)
+            {
+                button9.Text = "X";
+                label3.Text = "Następny ruch : O";
                 liczba_klikniec++;
             }
+
+            if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
+            {
+                label3.Text = " Gre wygrał gracz X";
+                blokada = false;
+            }
+
             liczba_ruchów++;
             label1.Text = " Liczba ruchów : " + liczba_ruchów;
-        }
 
+        }
         private void button8_Click(object sender, EventArgs e)
-        { 
-            if (liczba_klikniec % 2 == 0)
+        {
+
+
+            if (liczba_klikniec % 2 == 1 && button8.Text != "X"  && blokada == true)
+            {
+                button8.Text = "O";
+                liczba_klikniec++;
+                label3.Text = "Następny ruch : X";
+            }
+
+            if (liczba_klikniec % 2 == 0 && button8.Text != "O"  && blokada == true)
             {
                 button8.Text = "X";
                 label3.Text = "Następny ruch : O";
                 liczba_klikniec++;
             }
-            else if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
+
+            if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
             {
-                label3.Text = " Wygrał X";
+                label3.Text = " Gre wygrał gracz X";
+                blokada = false;
             }
-            else if (liczba_klikniec % 2 == 1)
-            {
-                button8.Text = "O";
-                label3.Text = "Następny ruch : X";
-                liczba_klikniec++;
-            }
+
             liczba_ruchów++;
             label1.Text = " Liczba ruchów : " + liczba_ruchów;
+
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            if (liczba_klikniec % 2 == 0)
+
+
+            if (liczba_klikniec % 2 == 1 && button7.Text != "X"  && blokada == true)
+            {
+                button7.Text = "O";
+                liczba_klikniec++;
+                label3.Text = "Następny ruch : X";
+            }
+
+            if (liczba_klikniec % 2 == 0 && button7.Text != "O"  && blokada == true)
             {
                 button7.Text = "X";
                 label3.Text = "Następny ruch : O";
                 liczba_klikniec++;
             }
-            else if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
+
+            if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
             {
-                label3.Text = " Wygrał X";
+                label3.Text = " Gre wygrał gracz X";
+                blokada = false;
             }
-            else if (liczba_klikniec % 2 == 1)
-            {
-                button7.Text = "O";
-                label3.Text = "Następny ruch : X";
-                liczba_klikniec++;
-            }
+
             liczba_ruchów++;
             label1.Text = " Liczba ruchów : " + liczba_ruchów;
+
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -255,7 +298,6 @@ namespace GraKółkoIKrzyżyk
         private void button10_Click(object sender, EventArgs e)
         {
             label3.Text = "Trwa rozgrywka ... ";
-          
         }
         private void label1_Click(object sender, EventArgs e)
         {
